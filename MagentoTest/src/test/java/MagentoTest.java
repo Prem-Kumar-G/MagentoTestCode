a50e635fbed2107a468590302d1cb2c5daa117b4
Prem G <Prem G@DESKTOP-26L1V7U>
2018-10-13 12:25:47 +0530

    1st push

---
+++ MagentoTest/src/test/java/MagentoTest.java
@@ -0,0 +1,40 @@
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class MagentoTest {
+
+	
+	
+	@Test
+	public  void posCredentials() {
+
+	String url = "https://magento.com/";
+	WebDriver  d = new ChromeDriver();
+	d.manage().window().maximize();
+	d.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+	
+	d.get(url);
+	
+		Main m = new Main(d);
+		m.clickOnMyacc();
+		
+		Login l1 = new Login(d);
+		l1.typeEmail("umashankar.h91@gmail.com");
+
+		l1.typePwd("Ascetic123456");
+
+		
+		l1.clickLogin();
+		
+		Logout l2 = new Logout(d);
+		l2.clikLogout();
+		
+		d.quit();
+	}
+
+}
